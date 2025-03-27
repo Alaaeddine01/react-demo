90afeab01a3b55580006064f37a3fa4dc1fd26f2
Alaaeddine elhaouch <alaaeddine958@gmail.com>
2025-03-27 01:42:04 +0000

    add TableItem component

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RowItem from './components/RowItem'
+import TableItem from './components/TableItem'
 
 function App() {
 
@@ -10,42 +11,20 @@ function App() {
     {num:5,nom:'alaoui',prenom:'mohamed'},
     {num:6,nom:'azami',prenom:'yassine'},
     {num:5,nom:'reda',prenom:'mohamed'},
-
   ]
   
   return (
     <div>
 
-      <table>
 
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-          </tr>
-        </thead>
+      <TableItem persons={persons}/>
 
-
-        <tbody>
-
-          <tr>
-            <th>1</th>
-            <th>EL HAOUCH</th>
-            <th>Alaaeddine</th>
-          </tr>
-        
-         <RowItem num_key={persons[0].num} nom_key={persons[0].nom} prenom_key ={persons[0].prenom}/>
-         <RowItem num_key={persons[1].num} nom_key={persons[1].nom} prenom_key ={persons[1].prenom}/>
-         
-
-
-        </tbody>
+      
 
 
 
 
-      </table>
+
 
 
 
